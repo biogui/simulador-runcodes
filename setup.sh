d4525a4908264d60183ba1f09838cb934677acd1
biogui <gmrtri@hotmail.com>
2020-09-02 15:21:32 -0300

    Rewrite setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mkdir ~/rcsimulator
-mv rcsimulator.py ~/rcsimulator
-chmod 744 ~/rcsimulator/rcsimulator.py
-sudo ln -s ~/rcsimulator/rcsimulator.py /usr/bin/rcsim
-mv setup.sh ~/rcsimulator
+chmod 744 ~/.rcsimulator/rcsimulator.py
+sudo ln -s ~/.rcsimulator/rcsimulator.py /usr/bin/rcsim
